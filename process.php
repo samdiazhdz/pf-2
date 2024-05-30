15f723f061066dde45fd10a44be248f53780d8eb
Sam Diaz <86027077+samdiazhdz@users.noreply.github.com>
2024-05-30 12:41:04 -0600

    Update process.php

--- process.php
+++ process.php
@@ -57,7 +57,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             }
         }
     } else {
-        header("Location: index.php");
+        header("Location: index.html");
         exit;
     }
 }
